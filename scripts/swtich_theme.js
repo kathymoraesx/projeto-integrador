a86d175e00909e216ca177c8bb2c047a6d59b5b6
kathelyn_andrade23@icloud.com <50355266822@>
2024-12-05 16:44:27 -0300

    alteraçao de dark/light mode

--- scripts/swtich_theme.js
+++ scripts/swtich_theme.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-
+document.addEventListener("DOMContentLoaded", () => {
     const theme_switch = document.getElementById("switch-theme");
-    const body = document.body
-    const sun = document.getElementById("sun")
-    const moon = document.getElementById("moon")
-
-    theme_switch.addEventListener("click", ()=>{
-        sun.style.display = sun.style.display === "none" ? "block": "none";
-        moon.style.display = moon.style.display === "block" ? "none": "block";
-    })
-})
+    const sun = document.getElementById("sun");
+    const moon = document.getElementById("moon");
+    const logo = document.getElementById("image-logo");
+  
+    // Caminhos das imagens
+    const logoLight = "logoborboleta.png"; // Nome da imagem Light Mode
+    const logoDark = "darkmode.png";      // Nome da imagem Dark Mode
+  
+    theme_switch.addEventListener("click", () => {
+      // Alterna os ícones de sol e lua
+      sun.style.display = sun.style.display === "none" ? "block" : "none";
+      moon.style.display = moon.style.display === "none" ? "block" : "none";
+  
+      // Alterna o logo com base no tema
+      logo.src = logo.src.includes(logoLight) ? "/imagens esmaltes/" + logoDark : "/imagens esmaltes/" + logoLight;
+    });
+  });
